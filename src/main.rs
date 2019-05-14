7e90f4e4d532ec750152889ea5c8ae6ccd076fe9
Laurențiu Nicola <lnicola@dend.ro>
2019-05-14 21:10:35 +0300

    Refactor and use mmap

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use byteorder::{NativeEndian, WriteBytesExt};
+use byteorder::{ByteOrder, NativeEndian};
 use goblin::error::Result;
 use goblin::Object;
+use memmap::MmapOptions;
 use std::env;
-use std::fs;
+use std::fs::{self, OpenOptions};
+use std::ops::Range;
 
 #[link_section = "count"]
-#[no_mangle]
+#[used]
 static mut RUN_COUNT: u32 = 0;
+
+fn get_section(obj: &Object, name: &str) -> Option<Range<usize>> {
+    if let Object::Elf(elf) = obj {
+        for section in &elf.section_headers {
+            if &elf.shdr_strtab[section.sh_name] == name {
+                return Some(section.file_range());
+            }
+        }
+    }
+    None
+}
 
 fn main() -> Result<()> {
     let run_count = unsafe { RUN_COUNT };
     println!("Previous run count: {}", run_count);
     let exe = env::current_exe()?;
-    let mut buffer = fs::read(&exe)?;
+    let tmp = exe.with_extension("tmp");
+    fs::copy(&exe, &tmp)?;
 
-    let mut range = None;
-    if let Object::Elf(elf) = Object::parse(&buffer)? {
-        for section in elf.section_headers {
-            let name = &elf.shdr_strtab[section.sh_name];
-            if name == "count" {
-                range = Some(section.file_range());
-                break;
-            }
-        }
-    }
+    let file = OpenOptions::new().read(true).write(true).open(&tmp)?;
+    let mut buf = unsafe { MmapOptions::new().map_mut(&file) }?;
 
-    if let Some(range) = range {
-        let tmp = exe.with_extension("tmp");
-        let mut buf = &mut buffer[range];
-        buf.write_u32::<NativeEndian>(run_count + 1)?;
-        fs::write(&tmp, &buffer)?;
+    let obj = Object::parse(&buf)?;
+    if let Some(range) = get_section(&obj, "count") {
+        NativeEndian::write_u32(&mut buf[range], run_count + 1);
+
         let perms = fs::metadata(&exe)?.permissions();
         fs::rename(&tmp, &exe)?;
         fs::set_permissions(&exe, perms)?;
